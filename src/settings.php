626cd57bb4948b513159d296beaef1fedadb9c6b
aquilax <aquilax@gmail.com>
2016-04-02 11:01:59 +0200

    Initial commit

---
+++ src/settings.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+
+        // View settings
+        'view' => [
+            'template_path' => __DIR__ . '/../templates/',
+            'cache_path' => __DIR__ . '/../cache/',
+        ],
+    ],
+];
